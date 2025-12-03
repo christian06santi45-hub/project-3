483c3fd2cf7103d1b77ed51b53fb348dd91e3179
christian06santi45-hub <christian06santi45@gmail.com>
2025-12-02 19:57:48 -0600

    push

--- index.js
+++ index.js
@@ -1,37 +1,54 @@
 //curl -H "x-api-key: live_8q3IvbAUEkjdrlTHgtNmDeQnrqyUz6RWiC7F7jTxi44yPLwRRrxAFIrVSVG6chWm" "https://api.thedogapi.com/v1/images/search?limit=2"
 //curl -H "x-api-key: live_8q3IvbAUEkjdrlTHgtNmDeQnrqyUz6RWiC7F7jTxi44yPLwRRrxAFIrVSVG6chWm" "https://api.thedogapi.com/v1/breeds"
-const listEl = document.querySelector('.dog__list')
+let allBreeds = [];
+    const listEl = document.querySelector('.dog__list')
+    const searchbar = document.getElementById('search__input');
 
-async function main() {
-    const breedsRes = await fetch("https://api.thedogapi.com/v1/breeds?limit=20");
-    const breedsData = await breedsRes.json();
-    listEl.innerHTML = breedsData.map((breed) => breedHTML(breed)).join("");
-}
+    searchbar.addEventListener('input', (e) => {
+        const searchString = e.target.value.toLowerCase();
+        const filteredBreeds = allBreeds.filter((breed) => {
+            return breed.name.toLowerCase().includes(searchString);
+        });
+        
+        displayBreeds(filteredBreeds);
+    });
 
-function breedHTML(breed) {
-    const breedGroup = breed.breed_group || 'N/A'
-    const imageUrl = breed.reference_image_id 
-        ? `https://cdn2.thedogapi.com/images/${breed.reference_image_id}.jpg`
-        : '';
+    async function main() {
+        // ... rest of your main function
+        const breedsRes = await fetch("api.thedogapi.com");
+        const breedsData = await breedsRes.json();
+        allBreeds = breedsData; 
+        displayBreeds(allBreeds);
+    }
 
-    return `<div class="dog__card">
-        <figure class="dog__img--wrapper">
-            <img class="dog__img" src="${imageUrl}" alt="A ${breed.name}">
-        </figure>
-        <div class="dog__details">
-            <div class="dog__title">
-                ${breed.name}
+    function displayBreeds(breedsToDisplay) {
+        // ... rest of your displayBreeds function
+        listEl.innerHTML = breedsToDisplay.map((breed) => breedHTML(breed)).join("");
+    }
+
+    function breedHTML(breed) {
+        // ... rest of your breedHTML function
+        const breedGroup = breed.breed_group || 'N/A';
+        const imageUrl = breed.image?.url ?? '';
+
+        return `<div class="dog__card">
+            <figure class="dog__img--wrapper">
+                <img class="dog__img" src="${imageUrl}" alt="A ${breed.name}">
+            </figure>
+            <div class="dog__details">
+                <div class="dog__title">
+                    ${breed.name}
+                </div>
+                <div class="dog__breed">
+                    <ol class="dogs__breed--list">
+                        <li><b>Height: </b>${breed.height.imperial} inches</li>
+                        <li><b>Weight: </b>${breed.weight.imperial} lbs</li>
+                        <li><b>Group: </b>${breedGroup}</li> 
+                    </ol>
+                </div>
             </div>
-            <div class="dog__breed">
-                <ol class="dogs__breed--list">
-                    <li><b>Height: </b>${breed.height.imperial} inches</li>
-                    <li><b>Weight: </b>${breed.weight.imperial} lbs</li>
-                    <li><b>Group: </b>${breed.breedGroup}</li>
-                </ol>
-            </div>
-        </div>
-    </div>`;
-}
+        </div>`;
+    }
 
-main();
+    main();
     
