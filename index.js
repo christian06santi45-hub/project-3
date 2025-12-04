9d9acea8e9a7f63b0d20b7bfaf5197344a4f1c1d
christian06santi45-hub <christian06santi45@gmail.com>
2025-12-03 20:33:45 -0600

    push

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
-//curl -H "x-api-key: live_8q3IvbAUEkjdrlTHgtNmDeQnrqyUz6RWiC7F7jTxi44yPLwRRrxAFIrVSVG6chWm" "https://api.thedogapi.com/v1/images/search?limit=2"
-//curl -H "x-api-key: live_8q3IvbAUEkjdrlTHgtNmDeQnrqyUz6RWiC7F7jTxi44yPLwRRrxAFIrVSVG6chWm" "https://api.thedogapi.com/v1/breeds"// 
+//curl -H "x-api-key: YOUR_KEY" "https://api.thedogapi.com/v1/images/search?limit=2"
+//curl -H "x-api-key: YOUR_KEY" "https://api.thedogapi.com/v1/breeds"// 
+
 let allBreeds = [];
 
- const listEl = document.querySelector('.dog__list');
- const searchbar = document.querySelector('.search__input');
+const listEl = document.querySelector('.dog__list');
+const searchbar = document.querySelector('.search__input');
 
 searchbar.addEventListener('input', (e) => {
-    const searchString = e.target.value.toLowerCase();
+  const searchString = e.target.value.toLowerCase();
 
-    const filteredBreeds = allBreeds.filter((breed) =>
-        breed.name.toLowerCase().includes(searchString)
-    );
+  const filteredBreeds = allBreeds.filter((breed) =>
+    breed.name.toLowerCase().includes(searchString)
+  );
 
-    displayBreeds(filteredBreeds);
+  displayBreeds(filteredBreeds);
 });
 
 async function main() {
   const breedsRes = await fetch("https://api.thedogapi.com/v1/breeds?limit=20");
   const breedsData = await breedsRes.json();
+  console.log(breedsData);
 
-  allBreeds = breedsData;
+
+  const breedsWithImages = await Promise.all(
+    breedsData.map(async (breed) => {
+      if (breed.reference_image_id) {
+        const imgRes = await fetch(`https://api.thedogapi.com/v1/images/${breed.reference_image_id}`);
+        const imgData = await imgRes.json();
+        breed.imageUrl = imgData.url;       
+      } else {
+        breed.imageUrl = "https://via.placeholder.com/300?text=No+Image";
+      }
+      return breed;
+    })
+  );
+
+  allBreeds = breedsWithImages;
   displayBreeds(allBreeds);
 }
 
@@ -28,33 +44,28 @@ function displayBreeds(breedsToDisplay) {
 }
 
 function breedHTML(breed) {
-    const breedGroup = breed.breed_group || 'N/A';
+  const breedGroup = breed.breed_group || "N/A";
 
-    const imageId = breed.reference_image_id;
-  const imageUrl = imageId
-    ? `https://cdn2.thedogapi.com/images/${imageId}.jpg`
-    : '';
+  const height = breed.height?.imperial || "N/A";
+  const weight = breed.weight?.imperial || "N/A";
 
-    const height = breed.height?.imperial || 'N/A';
-    const weight = breed.weight?.imperial || 'N/A';
-
-    return `
-        <div class="dog__card">
-            <figure class="dog__img--wrapper">
-                <img class="dog__img" src="${imageUrl}" alt="${breed.name}">
-            </figure>
-            <div class="dog__details">
-                <div class="dog__title">${breed.name}</div>
-                <div class="dog__breed">
-                    <ol class="dogs__breed--list">
-                        <li><b>Height:</b> ${height} inches</li>
-                        <li><b>Weight:</b> ${weight} lbs</li>
-                        <li><b>Group:</b> ${breedGroup}</li>
-                    </ol>
-                </div>
-            </div>
+  return `
+    <div class="dog__card">
+      <figure class="dog__img--wrapper">
+        <img class="dog__img" src="${breed.imageUrl}" alt="${breed.name}">
+      </figure>
+      <div class="dog__details">
+        <div class="dog__title">${breed.name}</div>
+        <div class="dog__breed">
+          <ol class="dogs__breed--list">
+            <li><b>Height: </b>${height} inches</li>
+            <li><b>Weight: </b>${weight} lbs</li>
+            <li><b>Group: </b>${breedGroup}</li>
+          </ol>
         </div>
-    `;
+      </div>
+    </div>
+  `;
 }
 
 main();
